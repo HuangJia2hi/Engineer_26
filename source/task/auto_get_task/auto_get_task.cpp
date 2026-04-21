d6b7ed0134bb7de547e4a4ca8257f9408c6064ce
huangjia2hi <huangjia2hi@163.com>
2026-04-21 21:26:41 +0800

    斜下取

--- source/task/auto_get_task/auto_get_task.cpp
+++ source/task/auto_get_task/auto_get_task.cpp
@@ -1,6 +1,7 @@
 #include "arm_state_machine.h"
 #include "auto_get_timer_init.h"
 #include "cmsis_os2.h"
+#include "ee_control_drv.h"
 #include "joint_control_drv.h"
 #include "stm32h7xx_hal_def.h"
 #include "tool.h"
@@ -135,12 +136,60 @@ traj_group_point_t traj_group_A[] = {
      GRIPPER_CLOSE_MODE,
     },
 };
+traj_group_point_t traj_group_B [] = {
+    
+    {{
+         {0, 0.5},
+         {0, 1.0},
+         {0.2, 1.0},
+         {0, 0.5},
+         {0, 0.5},
+         {0, 1.0},
+     },
+     2000,
+     GRIPPER_OPEN_MODE,
+    },
+    {{
+         {0, 0.5},
+         {0.7, 0.8},
+         {1.0, 1.0},
+         {0, 0.5},
+         {0, 0.5},
+         {Pi-0.90, 0.8},
+     },
+     5000,
+     GRIPPER_OPEN_MODE,
+    },
+    {{
+         {0.6, 0.5},
+         {0.7, 1.0},
+         {1.0, 1.0},
+         {0, 0.5},
+         {0, 0.5},
+         {Pi - 0.9, 1.0},
+     },
+     1500,
+     GRIPPER_CLOSE_MODE,
+    },
+    {{
+         {0, 1.0},
+         {0, 1.0},
+         {0.2, 1.0},
+         {0, 0.5},
+         {0, 0.5},
+         {0, 1.5},
+     },
+     1000,
+     GRIPPER_CLOSE_MODE,
+    },
+};
+
 TrajectoryExecutor traj_exec;
 
 uint32_t test_seq;
 extern "C" void auto_get_task(void *argument) {
   UNUSED(argument);
-  traj_exec.init(traj_group_A, TRAJ_GROUP_SIZE, 5);
+  traj_exec.init(traj_group_B, TRAJ_GROUP_SIZE, 5);
   traj_exec.build_time_acc();
   traj_exec.reset();
   while (true) {
