b0f66b9b5aed7dfc7f9573822d20097e1416d3cd
Mutilatedloud <2186157050@qq.com>
2026-05-20 20:43:36 +0800

    config

--- source/task/Chassis_Task/chassis_config.h
+++ source/task/Chassis_Task/chassis_config.h
@@ -9,7 +9,7 @@
 #define Max_Velocity 5.0f                                     /* 底盘平移控制的最大线速度 */
 #define Chassis_Keyboard_Shift_X_Axis_Speed_Ratio 0.1f       /* Shift 下映射到底盘 X 轴的速度倍率：前方向 WS / 右方向 AD 共用 */
 #define Chassis_Keyboard_Shift_Y_Axis_Speed_Ratio 0.8f       /* Shift 下映射到底盘 Y 轴的速度倍率：前方向 AD / 右方向 WS 共用 */
-#define Chassis_Keyboard_Shift_Yaw_Speed_Ratio Chassis_Keyboard_Shift_X_Axis_Speed_Ratio /* 键盘按下 Shift 时的旋转速度倍率 */
+#define Chassis_Keyboard_Shift_Yaw_Speed_Ratio 0.3 /* 键盘按下 Shift 时的旋转速度倍率 */
 #define Chassis_Keyboard_Translation_Accel_Max 12.0f         /* 键盘平移缓启动斜率上限，单位 m/s^2 */
 #define Chassis_Keyboard_Translation_Decel_Max 18.0f         /* 键盘平移缓停斜率上限，单位 m/s^2 */
 #define Chassis_Lateral_Forward_Compensation_Ratio 0.08f     /* 左右平移时补一点前向量，抵消底盘轻微后溜 */
